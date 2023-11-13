730f2d67875a36d238faf2d47a674b924e87b50c
Anthony Yatabe <anthonypiresdm@gmail.com>
2023-11-13 13:57:42 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,6 +1,4 @@
-﻿using xadrez_console.tabuleiro;
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     internal class Tabuleiro
     {
@@ -13,6 +11,11 @@ namespace tabuleiro
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna) 
+        {
+            return pecas[linha, coluna];
         }
     }
 }
