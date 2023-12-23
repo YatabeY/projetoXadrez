a4c41847bc76339b9263b8e5a1109e1501254129
Anthony Yatabe <anthonypiresdm@gmail.com>
2023-12-23 19:09:48 -0300

    jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -130,6 +130,24 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //jogadaEspecial promocao
+
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
                 xeque = true;
             else
@@ -144,7 +162,6 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
 
             // #jogadaEspecial EnPassat
             if (p is Peao && (destino.linha == origem.linha - 2) || destino.linha == origem.linha + 2)
